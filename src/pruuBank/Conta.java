fdf530cc317a54065264a0b0fca5f41d594d165c
gustavoufrn <mxgugaa@gmail.com>
2026-04-21 21:04:29 -0300

    getters e setters contaAbstrata

--- src/pruuBank/Conta.java
+++ src/pruuBank/Conta.java
@@ -3,6 +3,36 @@ package pruuBank;
 public abstract class Conta {
 	
 	private String numeroConta;
-	
+	private double saldo;
 	private Cliente cliente;
+	
+	public Conta(String nome, String numeroConta, double saldo, Cliente cliente) {
+		this.numeroConta = numeroConta;
+		this.saldo = saldo;
+		this.cliente = cliente;
+	}
+	
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 }
